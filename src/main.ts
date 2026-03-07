ed810df78c6e31f622338c111ca3a7591e8c1407
unknown <aldmyatyfrhh@gmail.com>
2026-03-07 06:51:30 +0200

    setting of CROS

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,11 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api/v1')
 
   app.use(helmet())
